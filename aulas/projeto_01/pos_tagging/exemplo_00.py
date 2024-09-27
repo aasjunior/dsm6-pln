93369a893a92e9ec5323b4e5739c7503e3045a2d
aasjunior <adeldivo@hotmail.com>
2024-09-26 22:38:24 -0300

    projeto_01

--- aulas/projeto_01/pos_tagging/exemplo_00.py
+++ aulas/projeto_01/pos_tagging/exemplo_00.py
@@ -1,5 +1,25 @@
 import nltk
 
-word = 'possuir'
+# Técnica para entender a mnatureza das palavras presentes no corpus
+# Classificação das palavreas por meio da classe gramatical
 
-word = nltk.word_tokenize()
+text = 'O rato roeu a roupa do rei de Roma.'
+
+words = nltk.word_tokenize(text.lower())
+
+stopwords = nltk.corpus.stopwords.words('portuguese')
+
+responses = []
+
+for word in words:
+    if not (word in stopwords):
+        responses.append(word)
+
+print(responses)
+
+# Entender o significado das palavras - Analise semantica
+categories = nltk.pos_tag(responses)
+
+pos_tagging = nltk.ne_chunk(categories)
+
+print(pos_tagging)
